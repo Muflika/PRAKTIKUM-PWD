105b42ba944b61e856717ca65a16d8faf90016a7
Muflika rahmadani <50790174+Muflika@users.noreply.github.com>
2021-11-26 19:06:05 +0700

    PRAKTIKUM 6

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
-    session_destroy();
-    echo "Anda telah sukses keluar sistem <b>LOGOUT</b>";
+session_start(); 
+session_destroy();
+	echo "Anda telah sukses keluar sistem <b>LOGOUT</b>";
 ?>
